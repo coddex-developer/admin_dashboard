3f64c7c2f9f3847d9da6e439fd52fffcb6db3f7a
CoddeX Developer <gabrielrodrigueslima22@gmail.com>
2025-05-14 19:28:56 -0300

    Adiciona página de edição de produtos com formulário para atualizar informações. Cria estilos para a nova página e melhora a estrutura visual do formulário.

--- src/components/pages/EditProduct/index.jsx
+++ src/components/pages/EditProduct/index.jsx
@@ -1,8 +1,27 @@
+import Navbar from "../../Navbar";
+import "./styles.min.css";
 export default function EditProduct() {
-    return(
+    return (
         <>
-            <h1>Edição de produtos</h1>
-            <p>Pagina em construção</p>
+            <Navbar />
+            
+            <div className="containerEditProduct">
+                <form>
+                    <h1>Atualizar Produto</h1>
+                    <label htmlFor="nome">Url da Imagem:</label>
+                    <input type="text" name="" id="nome" placeholder="Url da Imagem" />
+                    
+                    <label htmlFor="titulo">Título:</label>
+                    <input type="text" name="titulo" id="titulo" placeholder="Título" />
+                    
+                    <label htmlFor="preco">Preço:</label>
+                    <input type="text" name="preco" id="preco" placeholder="Preço $" />
+                    
+                    <label htmlFor="descricao">Descrição:</label>
+                    <textarea name="descricao" id="descricao" placeholder="Descrição..."></textarea>
+                    <button type="submit">Atualizar</button>
+                </form>
+            </div>
         </>
     );
 }
